61d28aed81764b697cac0bace716e9ea925da89e
Alessia Tiraboschi <alessia.tiraboschi@itsrizzoli.it>
2023-12-18 11:04:29 +0100

    sviluppato input utente

--- src/BinaryToDecimo.java
+++ src/BinaryToDecimo.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class BinaryToDecimo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner scanner = new Scanner (System.in);
+
+       System.out.println("inserisci stringa binaria: ");
+       String binarystrString = scanner.nextLine();
+
+       int decimalValue = 0;
+       // int decimalvalue = binarytodecimo (binarystring);
+       System.out.println("valore decimale"+ decimalValue);
+       scanner.close();
     }
 }
