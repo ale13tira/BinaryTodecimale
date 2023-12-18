0d1cbe854abecf5fadd97169848b2e9e6582d5c8
Alessia Tiraboschi <alessia.tiraboschi@itsrizzoli.it>
2023-12-18 12:07:04 +0100

    funzione di conversione

--- src/BinaryToDecimo.java
+++ src/BinaryToDecimo.java
@@ -2,4 +2,19 @@ public class BinaryToDecimo {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static int binaryToDecimal(String binaryString)  {
+        int decimalValue = 0;
+
+        int len = binaryString.length();
+
+        for (int i=0; i < binaryString.length(); i++)  {
+            int digit = Character.getNumericValue(binaryString.charAt(i));
+            int power = len - i - 1;
+            decimalValue = decimalValue + (int)(digit * Math.pow(2, power));
+
+        }
+
+        return decimalValue;
+    }
 }
